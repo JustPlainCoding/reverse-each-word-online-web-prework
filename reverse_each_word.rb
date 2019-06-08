20b7a276c68ea1b57a139f65508b7dfa35f409a4
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-08 16:17:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(string)
   reversed = []
   word_array = string.split(" ")
   word_array.each{|word| reversed << word.reverse}
+  puts "#{reversed}"
   reversed.join(' ')
   reversed
 end
